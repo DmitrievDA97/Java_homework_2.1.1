df4f14c183d30ad8716a8de406e2c8bc4f8d902a
Denis Dmitriev <denitos97@gmail.com>
2021-01-19 10:56:16 +0300

    Update app

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5000;
+        int miles = ticketPrice / 20;
+        System.out.println(miles);
+    }
+}
--- src/main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class main {
-    public static void main(String[] args) {
-        int ticket_price = 5000;
-        int miles = ticket_price / 20;
-        System.out.println(miles);
-    }
-}
