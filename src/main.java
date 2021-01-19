a63d024b24520f5c2284b6b77bf94b19d44157cc
Denis Dmitriev <denitos97@gmail.com>
2021-01-19 09:47:07 +0300

    Added Miles

---
+++ src/main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        int ticket_price = 5000;
+        int miles = ticket_price / 20;
+        System.out.println(miles);
+    }
+}
